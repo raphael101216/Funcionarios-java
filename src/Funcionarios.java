d6c2234427d95bc903a13e76d4871a6635328860
raphael101216 <raphaelrock_8@hotmail.com>
2020-05-29 19:10:09 -0300

    Projeto criado

---
+++ src/Funcionarios.java
@@ -0,0 +1,15 @@
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Funcionarios {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.00");
+
+	}
+
+}
